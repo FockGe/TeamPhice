79e9eec640966301c1e785f2df114a2e8b2bfa11
KAINANIK <40146720140048@litec.ac.at>
2018-10-09 16:12:17 +0200

    android projekt erstellt

---
+++ app/src/main/java/at/ac/litec/phice/auva/MainActivity.java
@@ -0,0 +1,13 @@
+package at.ac.litec.phice.auva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
